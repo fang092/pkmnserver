69ae10adddca6fd3d7414d5b909e69d08b0c7bf8
fang092 <bradleyg36@gmail.com>
2022-07-23 19:40:32 -0400

    added logos and favicon

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,11 +1,12 @@
 import React from 'react'
-import { Link } from 'react-router-dom'
+import { Link } from 'react-router-dom';
+import logo from '../Pokelogo.svg';
 
 export default function Navbar() {
   return (
     <div className='w-full h-12  bg-rosemadder flex flex-row  items-center p-6 mb-7'>
         <h4>
-            <Link className='text-platnium' to="/">PokeCodex</Link> 
+            <Link className='text-platnium' to="/"><img src={logo} className="w-10 h-10" alt="logo"/></Link> 
         </h4>
     </div>
   )
