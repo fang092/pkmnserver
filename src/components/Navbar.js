dac514e206dfc3a0d72e7efcd3e4d7bb584ca49b
fang092 <bradleyg36@gmail.com>
2022-07-23 20:40:14 -0400

    made a back button

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,13 +1,16 @@
 import React from 'react'
 import { Link } from 'react-router-dom';
-import logo from '../Pokelogo.svg';
+import logo from '../images/Pokelogo.svg';
+import bglogo from '../images/logo_md.svg';
+import leftarrow from '../images/leftarrow.png';
 
 export default function Navbar() {
   return (
-    <div className='w-full h-12  bg-rosemadder flex flex-row  items-center p-6 mb-7'>
-        <h4>
-            <Link className='text-platnium' to="/"><img src={logo} className="w-10 h-10" alt="logo"/></Link> 
-        </h4>
+    <div className='w-full h-12  bg-rosemadder flex flex-row justify-between items-center p-6 mb-7'>
+      <h4>
+        <Link className='text-platnium flex flex-row justify-center items-center' to="/"><img src={logo} className="w-10 h-10" alt="logo"/>PokeCodex</Link> 
+      </h4>
+      <a className='text-platnium flex flex-row justify-center items-center' href='https://bradgrad.design/#projects'><img src={leftarrow} alt="Left Arrow" /><img src={bglogo} className="w-10 h-10" alt="logo"/> </a> 
     </div>
   )
 }
